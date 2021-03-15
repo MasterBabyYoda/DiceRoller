917daffa1f3e3fc1a32d2d595865be08e4abfe25
master baby yoda <josephkhowerton@gmail.com>
2021-03-15 16:33:17 -0700

    App Level Gradle was updated to use the support library

--- app/src/main/java/com/josephhowerton/diceroller/FirstFragment.kt
+++ app/src/main/java/com/josephhowerton/diceroller/FirstFragment.kt
@@ -6,12 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
+import kotlin.random.Random
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+
+    lateinit var diceImage: ImageView
+    lateinit var rollButton: Button
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +28,21 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        diceImage = view.findViewById(R.id.dice)
+        rollButton = view.findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
         }
+    }
+    private fun rollDice() {
+        val drawableResource = when (Random.nextInt( 6)+ 1){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource);
     }
 }
